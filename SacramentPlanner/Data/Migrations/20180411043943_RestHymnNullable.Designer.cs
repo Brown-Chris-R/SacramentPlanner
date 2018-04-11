82654779e6df91fa0a95e500815c10f0859ad0ad
Chris Brown <cbrown@weyerhaeuser.com>
2018-04-10 22:42:28 -0600

    Added the migration from Melissa's model change and updated the database

--- SacramentPlanner/Data/Migrations/20180411043943_RestHymnNullable.Designer.cs
+++ SacramentPlanner/Data/Migrations/20180411043943_RestHymnNullable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SacramentPlanner.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180411043943_RestHymnNullable")]
+    partial class RestHymnNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SacramentPlanner/Data/Migrations/20180411043943_RestHymnNullable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SacramentPlanner.Data.Migrations
+{
+    public partial class RestHymnNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IntermediateSongNumber",
+                table: "SacramentMeeting",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IntermediateSongNumber",
+                table: "SacramentMeeting",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
